bdb4f8c045a06a1d5a514938586a81e672caac9c
avdome <79293052+avdome@users.noreply.github.com>
2021-02-20 20:08:10 -0500

    Add files via upload

---
+++ hack/content.js
@@ -0,0 +1,20 @@
+console.log("hello world!!!!");
+
+let rawHtml = document.getElementsByTagName("p");
+var text = [];
+
+// console.log(rawHtml);
+
+for (let i = 0; i < rawHtml.length; i++) {
+    const element = rawHtml[i];
+    text = element.innerHTML;
+    
+    // element.innerHTML = "hacked";
+    // text.push(element.innerHTML);
+    console.log();  
+}
+/*
+for (let i = 0; i < text; i++) {
+    console.log(text[i]);
+}
+*/
